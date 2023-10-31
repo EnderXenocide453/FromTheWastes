1433eb27265969c3ed1a3dc3937e19beb24ecf22
EnderXenocide453 <lakssterius@gmail.com>
2023-10-31 13:46:05 +0300

    Модели ресурсов

---
+++ Assets/Scripts/Interactable/HintArea.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HintArea : InteractableObject
+{
+    protected override string Description => throw new System.NotImplementedException();
+
+    public override void StartInteract(Carrier carrier = null)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void StopInteract(Carrier carrier = null)
+    {
+        throw new System.NotImplementedException();
+    }
+}
